80a14ba371d00e29729ed582b5c8f4f7ca92d0da
ManuArugollu1 <55981203+ManuArugollu1@users.noreply.github.com>
2020-07-31 11:14:24 +0530

    spinner added

--- app/src/main/java/com/example/demoapplication/MainActivity.java
+++ app/src/main/java/com/example/demoapplication/MainActivity.java
@@ -1,36 +1,46 @@
 package com.example.demoapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.Serializable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.awt.font.NumericShaper;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+//import android.app.Activity;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener , View.OnClickListener {
     ArrayList<Map<String,Object>> data=new ArrayList<>();
     EditText name,rollno,searchRoll;
     TextView textView;
+
     static Context context;
     RadioGroup genderRadioGroup;
+    String year;
+
+
+
 //    int gender=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        context=this;
+        context = this;
         textView=findViewById(R.id.text);
         name=findViewById(R.id.name);
         rollno=findViewById(R.id.rollno);
@@ -42,6 +52,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         search.setOnClickListener(this);
         allDataBtn.setOnClickListener(this);
         genderRadioGroup=findViewById(R.id.gender);
+        Spinner spinner = findViewById(R.id.spinner);
+        spinner.setOnItemSelectedListener(this);
+
+        List<String> year = new ArrayList<String>();
+        year.add("Select one");
+        year.add("first Year");
+        year.add("second year");
+        year.add("Third year");
+        year.add("Final year");
+
+        ArrayAdapter<String> adapter = new  ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,year);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(adapter);
+
+
+
 //        genderRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
 //            @Override
 //            public void onCheckedChanged(RadioGroup radioGroup, int i) {
@@ -64,10 +92,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if(id!=-1) {
             obj.put("gender", id==R.id.male?1:2);
         }
+
+        obj.put("year", year);
         data.add(obj);
         name.setText("");
         rollno.setText("");
         genderRadioGroup.clearCheck();
+
+
     }
 
     public void searchData(){
@@ -85,7 +117,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Toast.makeText(this,"no data found",Toast.LENGTH_LONG).show();
     }
 
-    @Override
+
     public void onClick(View view) {
         if(view.getId()==R.id.btn){
             addData();
@@ -93,8 +125,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             searchData();
         } else{
             Intent intent=new Intent(this,MainActivity2.class);
-//            intent.putExtra("reference", (Parcelable) this);
+           intent.putExtra("reference", (Parcelable) this);
             startActivity(intent);
         }
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+        year = adapterView.getItemAtPosition(i).toString();
+
+        Toast.makeText(this,year,Toast.LENGTH_LONG).show();
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
